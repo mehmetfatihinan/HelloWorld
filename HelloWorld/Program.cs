f35619eea589a1901a290fadb9c807f3948ac507
mehmetfatihinan <mehmetfatihinan@gmail.com>
2024-12-19 10:30:08 +0300

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,3 @@
+﻿// Se// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Mehmet Fatih İnan" +
+    "Patika Plus programına katılma nedenim kendimi backend alanında geliştirmek ve yeni teknolojilere hızlı adapte olmak.");
