4cb54e97ff5f8a3a9d9696aa376da4085f6a8ce8
mehmetfatihinan <mehmetfatihinan@gmail.com>
2024-12-19 10:36:35 +0300

    Add class into the code and rewrite all lines

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,14 @@
-﻿Console.WriteLine("Mehmet Fatih İnan" +
-    "Patika Plus programına katılma nedenim kendimi backend alanında geliştirmek ve yeni teknolojilere hızlı adapte olmak.");
+﻿using System;
+
+namespace HelloWorldAssignment
+{
+    class App
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mehmet Fatih İnan " +
+                "Patika Plus programına katılma nedenim kendimi backend alanında geliştirmek ve yeni teknolojilere hızlı adapte olmak.");
+        }
+    }
+}
+
