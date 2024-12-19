a72512e317ae82e60351838af9aef52a72fb198f
mehmetfatihinan <mehmetfatihinan@gmail.com>
2024-12-19 10:32:50 +0300

    Remove Comments

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,3 +1,2 @@
-﻿// Se// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Mehmet Fatih İnan" +
+﻿Console.WriteLine("Mehmet Fatih İnan" +
     "Patika Plus programına katılma nedenim kendimi backend alanında geliştirmek ve yeni teknolojilere hızlı adapte olmak.");
